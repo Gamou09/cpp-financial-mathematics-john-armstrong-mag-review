68b930b21d14bd964754d088e1edd41511372c6c
Gamou09 <martial.gamouza@gmail.com>
2026-09-14 01:13:23 +0100

    Review Chapter 11 arrays, pointers, memory management, and smart pointers

---
+++ chap_11_mag/chap_11_Arrays_String_Pointers_myReview/chap_11_Arrays_String_Pointers_myReview/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chap_11_Arrays_String_Pointers_myReview
+//
+//  Created by Martial Aguessi on 12/09/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
